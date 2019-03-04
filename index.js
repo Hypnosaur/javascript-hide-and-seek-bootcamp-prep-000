efa2c3a3a74e3120c74681b0770519e5565aa58c
Laura Fretter <fretterlaura@gmail.com>
2019-03-04 20:23:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  var nested = document.getElementById("nested");
+  var target = nested.querySelector('.target');
+  return target;
+}
+
+function increaseRankBy(n){
+   var ranked = document.querySelectorAll(".ranked-list li")
+  for(var i = 0; i < ranked.length; i ++){
+    ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var deep = document.querySelectorAll("#grand-node div")
+  var deepest = deep[deep.length-1]
+  return deepest
+}
